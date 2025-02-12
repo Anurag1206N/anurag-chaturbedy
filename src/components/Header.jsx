1018a597e336deefbbf316316f195806cd820d6d
Anurag1206N <anuragchaturbedy12345@gmail.com>
2025-02-12 12:37:18 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2025 anuragchaturbedy
+ * @license Apache-2.0
+ */
+
+
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+        Header
+
+    </header>
+  )
+}
+
+export default Header
