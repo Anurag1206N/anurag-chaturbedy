d2fa18795f0f1a57b3fb85655ffc920048315788
Anurag1206N <anuragchaturbedy12345@gmail.com>
2025-03-27 04:37:49 +0530

    hero part update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
-import { letters } from '../data/index'
- 
+import { letters, professionTexts } from '../data/index' 
+
 const Hero = () => {
     const [hoveredLetter, setHoveredLetter] = useState(null)
+    const [currentText, setCurrentText] = useState(professionTexts[0])
+    const [isRotating, setIsRotating] = useState(false)
+    let currentTextIndex = 0
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsRotating(true)
+            setTimeout(() => {
+                currentTextIndex = (currentTextIndex + 1) % professionTexts.length
+                setCurrentText(professionTexts[currentTextIndex])
+                setIsRotating(false)
+            }, 300)
+        }, 5000)
+
+        return () => clearInterval(interval)
+    }, [])
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center'>
+    <div className='w-full h-screen flex flex-col justify-center items-center isolate'>
         <Navbar />
         <div className="flex flex-col md:items-center items-start xl:gap-y-10 gap-y-3 xl:mb-0 md:mb-20 mb-0">
             <h1 className='flex flex-col xl:space-y-8 md:space-y-4 space-y-2 xl:text-6xl md:text-4xl text-3xl md:font-normal font-bolder text-yellow-500'>
@@ -18,7 +34,18 @@ const Hero = () => {
                     </span>
                    ))}
                 </span>
+                <span className="xl:text-6xl md:text-4xl text-2xl tracking-wider xl:py-4 py-2 overflow-hidden">
+                    I'm{''} 
+                    <span className={`inline-block xl:w-[380px] md:w-[240px] w-[160px] lg:ml-6 ml-2 front-extrabold transform origin-left transition-transform duration-300 ease-out ${isRotating ? 'rotate-[100deg]' : 'rotate-0'}`}
+                    >
+                    {currentText} 
+                    </span>{''}
+                    Web Developer
+                </span>
             </h1>
+            <div className="lg:w-[600px md:w-[500px] w-[350px] absolute left-1/2 -translate-x-1/2 -z-10">
+            <img src="images/road.png" alt="Road Image" className='w-full mx-auto' />
+            </div>
         </div>
     </div>
   )
